d8f6e57c59e2d865fcd66798f3ea33c42c3b180e
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-12-10 03:30:11 -0600

    Initializes Day 2

---
+++ Day 2/sub_controls.rb
@@ -0,0 +1,17 @@
+def sub_controls
+    puts "Intializing Sub-Control System"
+    puts "\n 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 🔹 \n"
+    data = IO.readlines("input.txt")
+    postion = [0,0]
+
+end
+
+class MoveCommand
+    @
+    def initialize(direction, distance)
+        @direction = direction
+        @distance = distance
+    end
+end
+
+sub_controls
