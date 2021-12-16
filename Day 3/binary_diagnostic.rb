89c4bc923012262d556b11166f9e5ba90c87266c
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-12-16 11:50:13 -0600

    Adds some greek symbols do docs

--- Day 3/binary_diagnostic.rb
+++ Day 3/binary_diagnostic.rb
@@ -7,7 +7,7 @@ The submarine has been making some odd creaking noises, so you ask it to produce
 
 The diagnostic report (your puzzle input) consists of a list of binary numbers which, when decoded properly, can tell you many useful things about the conditions of the submarine. The first parameter to check is the power consumption.
 
-You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
+You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate 'γ' and the epsilon rate 'ε'). The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
 
 Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in the diagnostic report. For example, given the following diagnostic report:
 
